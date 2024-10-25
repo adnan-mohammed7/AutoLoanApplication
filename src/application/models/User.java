26c06bdd7b12339fb5d054955a3921917bce20cb
adnan-mohammed7 <mohammedadnanaz7@gmail.com>
2024-10-25 16:27:12 -0400

    User Class addded

---
+++ src/application/models/User.java
@@ -0,0 +1,18 @@
+package application.models;
+
+public class User {
+	String userName;
+	String password;
+	
+	User(String name, String pwd){
+		//No user validation is done as these are hard coded
+		this.userName = name;
+		this.password = pwd;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public String getPassword() {
+		return password;
+	}
+}
